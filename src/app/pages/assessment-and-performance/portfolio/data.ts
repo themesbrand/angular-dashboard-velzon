14d21ae07f8d132efd81734ca9b5ba3f3ea918e9
shanlakshitha94 <shanlakshitha94@hotmail.com>
2022-11-13 19:13:44 +0530

    Bug Fix

--- src/app/pages/assessment-and-performance/portfolio/data.ts
+++ src/app/pages/assessment-and-performance/portfolio/data.ts
@@ -5,21 +5,33 @@ const data = {
             omsbNumber : '19-251',
             residencyLevel: 'R3', 
             email: 'Falsal@email.com',
-            status : 'Progressing'
+            status : 'Progressing',
+            commentsFromTrainee: null,
+            commentsByAP: null,
+            progressSummary : 'not filled',
+            progressSummaryComments : null,
         },
         { 
             name: "Al Abri Ibraham", 
             omsbNumber : '20-601',
             residencyLevel: 'R3', 
             email: 'Ibraham@email.com', 
-            status : 'Under Remediation'
+            status : 'Under Remediation',
+            commentsFromTrainee: null,
+            commentsByAP: null,
+            progressSummary : 'not filled',
+            progressSummaryComments : null,
         },
         { 
             name: "Fahad Al Khatri", 
             omsbNumber : '20-221',
             residencyLevel: 'R3', 
             email: 'Khatri@email.com', 
-            status : 'Interrupted'
+            status : 'Interrupted',
+            commentsFromTrainee: null,
+            commentsByAP: null,
+            progressSummary : 'not filled',
+            progressSummaryComments : null,
         },
     ]
 }
