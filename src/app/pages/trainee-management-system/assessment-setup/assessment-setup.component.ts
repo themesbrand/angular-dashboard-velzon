c0a6b1aeb6b0682625ffff19c956e9d65e4a93f2
shanlakshitha94 <shanlakshitha94@hotmail.com>
2022-11-19 16:25:08 +0530

    Assessment Setup

--- src/app/pages/trainee-management-system/assessment-setup/assessment-setup.component.ts
+++ src/app/pages/trainee-management-system/assessment-setup/assessment-setup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-assessment-setup',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssessmentSetupComponent implements OnInit {
 
-  constructor() { }
+  breadCrumbItems!: Array<{}>;
+
+  formSpecific : any = null;
+  isApplicableFor : boolean = false;
+  
+  userRole: string | null = null;
+
+  constructor(
+    private router : Router,
+    private modalService : NgbModal
+   ) { }
 
   ngOnInit(): void {
+    this.breadCrumbItems = [
+      { label: "Trainee Management System", link: '/trainee-management-system'},
+      { label: "Assessment Setup ", active : true }
+    ];
+    this.userRole = localStorage.getItem('userType');
+  }
+
+  onChangeSpecific() {
+    if(this.formSpecific === 'Program Specific' ) {
+      this.isApplicableFor = true
+    } else {
+      this.isApplicableFor = false
+    } 
+  }
+
+  onNavigate(url : any) {
+    this.modalService.dismissAll();
+    this.router.navigateByUrl(url);
   }
 
 }
